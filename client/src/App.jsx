9cd012e031641652b4d0ee91d589196c7114fb8f
Louis Tsang <kawingt2@gmail.com>
2022-05-14 23:11:57 +0800

    set up Bootstrap and react-router in Client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,23 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './routes/Home'
+import RestaurantDetailPage from './routes/RestaurantDetailPage'
+import UpdatePage from './routes/UpdatePage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/restaurants/:id/update' element={<UpdatePage />} />
+          <Route exact path='/restaurants/:id' element={<RestaurantDetailPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
